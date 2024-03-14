af088555b0185e2bf7f79cfa7b088d67921a8619
Lgusta11 <rainlima202@gmail.com>
2024-03-14 13:49:41 -0300

    Api configurada

--- ScreenSound.Shared.Dados/Banco/DAL.cs
+++ ScreenSound.Shared.Dados/Banco/DAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace ScreenSound.Banco;
 public class DAL<T> where T : class
