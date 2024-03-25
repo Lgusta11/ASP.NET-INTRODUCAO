ec56a664a445427bf9c6e9affe7153fcf0cf643a
Lgusta11 <rainlima2020@gmail.com>
2024-03-25 17:57:43 -0300

    Como configurar a biblioteca MudBlazor para incrementar e otimizar a criação de componentes de interface para o Blazor;
    A customizar o tema de nossa interface no MudBlazor, deixando-a mais atraente;
    
    A utilizar um componente MudCard e a definir parâmetros nele;
    
    A definir o nosso CRUD para o endpoint de artista, consumindo nossa API e utilizando componentes prontos da biblioteca MudBlazor;
    
    Como implementar a paginação na listagem de artistas com o componente MudPagination, juntamente com a lógica para filtrar os dados e mudar de página.

---
+++ dbo.Artistas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Artistas] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]       NVARCHAR (MAX) NULL,
+    [FotoPerfil] NVARCHAR (MAX) NULL,
+    [Bio]        NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Artistas] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
